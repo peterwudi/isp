bb1f53a97406395992b7c9427f2502aa0d390b4f
peterwudi <peterwudi@gmail.com>
2013-11-12 18:22:45 -0500

    There's a weird bus warning, seems to be fine but
    Check on that anyways.

--- test/bus/tb.sv
+++ test/bus/tb.sv
@@ -12,17 +12,16 @@ module tb();
 logic					OSC2_50;
 logic					GPIO1_PIXLCLK;
 logic					ctrl_clk;
-//logic					dvi_clk;
 
 logic					reset_n;
-//logic					wr_new_frame;
-//logic					rd_new_frame;
 
 logic		[31:0]	iData;
 logic					sCCD_DVAL;
 	
 logic		[31:0]	Read_DATA;
-logic					read_init;
+
+logic					rCCD_FVAL;
+//logic					read_init;
 
 logic 				vpg_pclk;
 logic 				vpg_de;
@@ -45,14 +44,10 @@ bus dut ( .* );
 initial	GPIO1_PIXLCLK = '1;
 initial	ctrl_clk = '1;
 initial	OSC2_50 = '1;
-//initial	dvi_clk = '1;
 
 always begin
 	#20		GPIO1_PIXLCLK = ~GPIO1_PIXLCLK;  	// 25MHz d5m clock
 end
-//always begin
-//	#10		dvi_clk = ~dvi_clk;  	// 25MHz d5m clock
-//end
 
 always begin
 	#5		ctrl_clk = ~ctrl_clk;		// 100MHz ctrl clock
@@ -61,11 +56,14 @@ always begin
 	#10		OSC2_50 = ~OSC2_50;		// 50MHz ref clock
 end
 
+parameter	width = 640;
+parameter	height = 480;
+
 // Write
 initial begin
 	iData = 'd0;
 	sCCD_DVAL = 0;
-	//wr_new_frame = 0;
+	rCCD_FVAL = 0;
 	
 	@(negedge OSC2_50);
 	reset_n = 1'b0;
@@ -73,16 +71,30 @@ initial begin
 
 	reset_n = 1'b1;
 	
-//	for (int i = 1; i < 17; i++) begin
-//		@(negedge GPIO1_PIXLCLK);
-//	end
-	
-	sCCD_DVAL = 1'b1;
-	//wr_new_frame = 1;
-	for (int i = 1; i < 641; i++) begin
-		iData = i;
+	// give it some time, otherwise pre_rCCD_FVAL is always 1
+	for (int j = 0; j < 10; j++) begin
 		@(negedge GPIO1_PIXLCLK);
-		//wr_new_frame = 0;
+	end
+	rCCD_FVAL = 1;
+	
+	// FVAL is high for say 32 cycles before DVAL
+	for (int j = 0; j < 32; j++) begin
+		@(negedge GPIO1_PIXLCLK);
+	end
+	
+	for (int j = 0; j < height; j++) begin
+		sCCD_DVAL = 1'b1;
+		for (int i = 1; i < width+1; i++) begin
+			iData = iData+1;
+			@(negedge GPIO1_PIXLCLK);
+		end
+	
+		sCCD_DVAL = 0;
+		for (int i = 1; i < 40; i++) begin
+			@(negedge GPIO1_PIXLCLK);
+		end
+		
+		break;
 	end
 	
 	sCCD_DVAL = 0;
@@ -90,41 +102,55 @@ initial begin
 
 end
 
+logic hs;
+
+
 // Read
 initial begin
-	read_init = 0;
-	//rd_new_frame = 0;
-	@(negedge OSC2_50);
-	@(negedge OSC2_50);
-	
+	//read_init = 0;
+	hs = 0;
+	vpg_de = 0;
 	
 	// Use this to wait until gen_clk is locked.
 	// In reallity there's no read_init, use vga_de,
 	// which should be high after it's locked and skipped
 	// the first several rows.
-	for (int i = 0; i < 1255; i++) begin
+	for (int i = 0; i < 1550; i++) begin
 		@(negedge OSC2_50);
 	end
 	
-	//rd_new_frame = 1;
-	read_init = 1;
-	// Need 1 cycles for a valid data to be read
-	@(negedge vpg_pclk);
-	//rd_new_frame = 0;
-					
-	for (int j = 0; j < 20; j++) begin
-		for (int i = 0; i < 32; i++) begin
-			read_init = 1;
-			@(negedge vpg_pclk);
-			//rd_new_frame = 0;
-		end
-		
-		// Wait for say 10 cycles of front/back porch etc.
-		for (int i = 0; i < 32; i++) begin
-			read_init = 0;
-			@(negedge vpg_pclk);
+	// Do 1 row first
+	hs = 1;
+	while (1) begin
+		//if (vpg_hs == 1) begin
+		if (hs == 1) begin
+			// vpg_de is high 48 cycles after vpg_hs
+			for (int i = 0; i < 48; i++) begin
+				@(negedge vpg_pclk);
+			end
+			vpg_de = 1;
+			break;
 		end
 	end
+
+	for (int i = 0; i < 640; i++) begin
+		// vpg_de high for 640 cycles
+		@(negedge vpg_pclk);
+	end
+	vpg_de = 0;
+					
+//	for (int j = 0; j < height; j++) begin
+//		for (int i = 0; i < width; i++) begin
+//			read_init = 1;
+//			@(negedge vpg_pclk);
+//		end
+//		
+//		// Wait for say 32 cycles of front/back porch etc.
+//		for (int i = 0; i < 32; i++) begin
+//			read_init = 0;
+//			@(negedge vpg_pclk);
+//		end
+//	end
 	
 	for (int i = 0; i < 16; i++) begin
 		@(negedge vpg_pclk);
